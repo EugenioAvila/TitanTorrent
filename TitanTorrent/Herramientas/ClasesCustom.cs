0615ef876e4adcda721f0c8172ae688a421ce88f
Eugenio Avila <avilamqz@hotmail.com>
2018-05-22 14:38:46 -0700

    Cambios a consultas en pagina principal

---
+++ TitanTorrent/Herramientas/ClasesCustom.cs
@@ -0,0 +1,18 @@
+﻿namespace TitanTorrent.Herramientas
+{
+    public class ClasesCustom
+    {
+        public class CustomPlataformas
+        {
+            public string DESCRIPCION { get; set; }
+            public int ID { get; set; }
+        }
+
+        public class CustomCategorias
+        {
+            public string DESCRIPCION { get; set; }
+            public int ID { get; set; }
+            public int ID_PLATAFORMA { get; set; }
+        }
+    }
+}
--- TitanTorrent/MainWindow.xaml.cs
+++ TitanTorrent/MainWindow.xaml.cs
@@ -1,11 +1,59 @@
-﻿using System.Windows;
+﻿using System.Linq;
 namespace TitanTorrent
 {
     public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
     {
+        #region variables globales
+        System.Collections.Generic.List<Herramientas.ClasesCustom.CustomPlataformas> _plataformas;
+        System.Collections.Generic.List<Herramientas.ClasesCustom.CustomCategorias> _categorias;
+        #endregion
         public MainWindow()
         {
+            #region inicializa plataformas
+            _plataformas = new System.Collections.Generic.List<Herramientas.ClasesCustom.CustomPlataformas>();
+            _plataformas.Add(new Herramientas.ClasesCustom.CustomPlataformas() { DESCRIPCION = "CONCEN", ID = 1 });
+            #endregion
+
+            #region inicializa categorias
+            _categorias = new System.Collections.Generic.List<Herramientas.ClasesCustom.CustomCategorias>();
+            _categorias.Add(new Herramientas.ClasesCustom.CustomCategorias() { DESCRIPCION = "TODO", ID = 1, ID_PLATAFORMA = 1 });
+            #endregion
+
             InitializeComponent();
+
+            cmbPlataformas.DisplayMemberPath = "DESCRIPCION";
+            cmbPlataformas.ItemsSource = _plataformas;
+            
+        }
+
+        public static void InicializaCombos()
+        {
+            try
+            {
+
+            }
+            catch (System.Exception exc)
+            {
+                throw exc;
+            }
+        }
+
+        private void cmbPlataformas_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var _seleccion = ((sender as System.Windows.Controls.ComboBox).SelectedItem as Herramientas.ClasesCustom.CustomPlataformas);
+                if (_seleccion != null)
+                {
+                    cmbCategorias.DisplayMemberPath = "DESCRIPCION";
+                    cmbCategorias.ItemsSource = (from x in _categorias where x.ID_PLATAFORMA == _seleccion.ID select x);
+                    cmbCategorias.SelectedIndex = 0;
+                }
+            }
+            catch (System.Exception exc)
+            {
+                throw exc;
+            }
         }
     }
 }
